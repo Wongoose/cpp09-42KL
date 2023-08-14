b3844ebc3ea883863b80d2bff188912efe7468f4
Wongoose <wong.zhengxiang@gmail.com>
2023-08-14 17:19:14 +0800

    Start parsing BitcoinExchange

---
+++ ex00/BitcoinExchange.cpp
@@ -0,0 +1,84 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(std::string str) {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy) {
+	this->_exchangeRateMap = copy._exchangeRateMap;
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange other) {
+	this->_exchangeRateMap = other._exchangeRateMap;
+	return (*this);
+}
+
+void BitcoinExchange::_readCSV() {
+	std::ifstream file;
+	std::string buffer;
+
+	file.open("data.csv");
+	if (file.fail()) {
+		std::cout << "Failed to open file! Exiting program." << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	// still need to skip the first header
+	std::getline(file, buffer);
+	while(std::getline(file, buffer)) {
+		std::stringstream line(buffer);
+		std::string temp;
+		time_t dateTime;
+		float exchangeRate;
+		int i = 1;
+
+		while (std::getline(line, temp, ',')) {
+			switch (i) {
+				case 1:
+					dateTime = _convertDateFormat(temp);
+					break;
+				case 2:
+					exchangeRate = _convertExchangeRateToFloat(temp);
+					break;
+				default:
+					break;
+			}
+			i++;
+		}
+		_exchangeRateMap.insert({dateTime, exchangeRate}); // time_t, exchange rate
+	}
+}
+
+time_t BitcoinExchange::_convertDateFormat(std::string date) {
+	struct tm timeSet = {0};
+	std::string temp;
+	std::stringstream sdate(date);
+
+	int i = 1;
+
+	while (std::getline(sdate, temp, '-')) {
+		switch (i) {
+			case 1:
+				timeSet.tm_year = std::stoi(temp) - 1900;
+				break;
+			case 2:
+				timeSet.tm_mon = std::stoi(temp);
+				break;
+			case 3:
+				timeSet.tm_mday = std::stoi(temp);
+				break;
+
+			default:
+				break;
+		}
+		i++;
+	}
+
+	time_t timeSinceEpoch = mktime(&timeSet);
+	return (timeSinceEpoch);
+}
+
+float BitcoinExchange::_convertExchangeRateToFloat(std::string exchangeRate) {
+	return (std::stof(exchangeRate));
+}
---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,60 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+#define DEFAULT "\033[39m"
+#define BLACK "\033[30m"
+#define GREY "\033[90m"
+#define RED "\033[31m"
+#define CYAN "\033[36m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[94m"
+#define MAGENTA "\033[35m"
+
+// 2 files
+// 1. csv file has date & exchange rate
+// 2. input file has input date | amount
+// What do I need to do?
+// - Parsing (longest step, read, convert, get into map container)
+// - Calculations (find closes date, multiply exchange rate)
+// - Output (Print results, error handling)
+class BitcoinExchange {
+	public:
+		// Constructors
+		BitcoinExchange();
+		BitcoinExchange(std::string str);
+		BitcoinExchange(const BitcoinExchange &copy);
+
+		// Destructor
+		~BitcoinExchange();
+
+		// Operators
+		BitcoinExchange &operator=(BitcoinExchange other);
+
+		// Getters
+
+		// Setters
+
+		// Functions
+
+	private:
+		std::map<time_t, float> _exchangeRateMap;
+		// parsing
+		void _readCSV();
+		time_t _convertDateFormat(std::string date);
+		float _convertExchangeRateToFloat(std::string exchangeRate);
+		float _getExchangeRate();
+};
+
+#endif
